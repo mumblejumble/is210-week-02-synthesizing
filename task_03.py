93c9b73b2a3e7403f9ff59023702996ad2480f27
mumblejumble <danqing.lin@spsmail.cuny.edu>
2015-09-08 02:07:26 +0000

    made corrections by using pylint

--- task_03.py
+++ task_03.py
@@ -1,7 +1,7 @@
-CHARLIE = "Chocolate Factory" 
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+""" task 3 """
+CHARLIE = "Chocolate Factory"
 VIOLET = "Gray"
 PATRICIA = "Reichardt"
 LINUS = "van Pelt"
-
-
-
